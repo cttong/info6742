6ffa1c69bd3ff7ce5e85f269981b225322213b5b
Tom Davidson <trd54@cornell.edu>
2017-09-04 11:16:19 -0400

    Added functionality to resolve and store deltas

--- A1/data_exploration.py
+++ A1/data_exploration.py
@@ -7,10 +7,36 @@ import json
 import pickle
 
 def has_delta(comment_text):
+    """ This function returns True if the comment text contains a delta.
+    """
     if '∆' in comment_text:
         return True
     else:
         return False
+
+def resolve_deltas(comments):
+    """This function takes a set of comments in response to a post and
+    returns the usernames and comment IDs of the users who the OP
+    awarded a delta
+
+    Inputs:
+
+    list of dicts : Each dict contains info about a comment.
+
+    Returns:
+
+    list of tuples : Each tuple is an author name, comment ID pair."""
+    #TODO: This could be made more efficient by turning comments into a dict rather
+    # than a list of dicts.
+    D = []
+    for c in comments:
+        if c['author'] != "DeltaBot": # Ignore deltabot
+            if c['delta']:
+                parent = c['parent'].split('_')[1]
+                for k in comments: # loop through again to find parent
+                    if k['id'] == parent:
+                        D.append((k['author'],k['id']))
+    return D
 
 if __name__ == '__main__':
     data = glob.glob('../../CMV/*')[0] # Get string for JSON file with data
@@ -52,3 +78,14 @@ if __name__ == '__main__':
     # Store new version in pickle
     pickle.dump(posts_dict, open('post_info.p','wb'))
     pickle.dump(comments_dict, open('comment_info.p','wb'))
+
+    # Now resolving deltas and storing an object containing them
+    deltas_dict = {}
+    post_ids = list(posts_dict.keys())
+    for p in post_ids:
+        # Get comments for that post
+        deltas = resolve_deltas(comments_dict[p])
+        deltas_dict[p] = deltas
+
+    # Store deltas object in pickle
+    pickle.dump(deltas_dict, open('deltas_info.p','wb'))
